0642b1e80ee7237f59ae9719d6170a3e1fe37e47
shaydoc <>
2019-04-06 20:08:44 +0100

    init values

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
   },
   data() {
     return {
-      url: null,
+      url: "https://coub.com/view/1kur9x",
       embedUrl: "https://coub.com/view/1kur9x",
       showMediaFlag: true
     };
